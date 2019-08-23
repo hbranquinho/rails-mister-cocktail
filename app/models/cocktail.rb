f3ebf041b98de8034e7c37a36511a32af7b881bd
Hugo Branquinho <ohugobranquinho@icloud.com>
2019-08-23 18:10:54 -0300

    Update validate to photo

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -2,5 +2,6 @@ class Cocktail < ApplicationRecord
    mount_uploader :photo, PhotoUploader
    has_many :doses, dependent: :destroy
    has_many :ingredients, through: :doses
+   validates :photo, presence: true
    validates :name, presence: true, uniqueness: true
 end
